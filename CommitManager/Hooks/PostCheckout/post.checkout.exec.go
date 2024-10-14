3714bfb3179b84a1dde8257a2ad62ca76d144553
Vinay Singhal <vinay.singhal43@gmail.com>
2024-10-14 06:54:06 +0530

    git hooks scripts initial draft

---
+++ CommitManager/Hooks/PostCheckout/post.checkout.exec.go
@@ -0,0 +1,35 @@
+package postcheckout
+
+import (
+	"log"
+	"os"
+
+	Repository "GitHistoryTracker/DatabaseManager/Repository"
+
+	"github.com/fatih/color"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("No commit hashes provided")
+	}
+
+	commitHashes := os.Args[1:]
+
+	// connect to database
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		color.New(color.Bold, color.FgRed).Println("Error while connecting to DB:", err.Error())
+		return
+	}
+
+	var repository Repository.Repository
+	repository.Db = db
+
+	// delete commits
+	for _, commitHash := range commitHashes {
+		repository.Delete(commitHash)
+	}
+}
---
+++ CommitManager/Hooks/PostCommit/post.commit.exec.go
@@ -0,0 +1,54 @@
+package postcommit
+
+import (
+	"log"
+	"os"
+
+	Model "GitHistoryTracker/DatabaseManager/Models"
+	Repository "GitHistoryTracker/DatabaseManager/Repository"
+
+	"github.com/fatih/color"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// get commit hash from OS
+	if len(os.Args) < 2 {
+		log.Fatal("No commit hash provided")
+	}
+
+	commitHash := os.Args[1]
+
+	// get commit info
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		color.New(color.Bold, color.FgRed).Println("Could not open repository:", err.Error())
+		return
+	}
+
+	commit, err := repo.CommitObject(plumbing.NewHash(commitHash))
+	if err != nil {
+		color.New(color.Bold, color.FgRed).Println("Could not find commit:", err.Error())
+		return
+	}
+
+	// connect to database
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		color.New(color.Bold, color.FgRed).Println("Error while connecting to DB:", err.Error())
+		return
+	}
+
+	// save commit
+	commitObj := Model.CommitHistory{
+		Date:       commit.Author.When,
+		CommitHash: commitHash,
+	}
+	var repository Repository.Repository
+	repository.Db = db
+	repository.Insert(commitObj)
+
+}
