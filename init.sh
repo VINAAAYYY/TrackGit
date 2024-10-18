631e7b145963b6d4c667f004669e4fcbc7581f27
Vinay Singhal <vinay.singhal43@gmail.com>
2024-10-19 01:13:58 +0530

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-export TRACKGIT_PATH=$(pwd)
+TRACKGIT_PATH=$(pwd)
+
+if [ -f ~/.bash_profile ]; then
+    if ! grep -q "source ~/.bashrc" ~/.bash_profile; then
+        echo "source ~/.bashrc" >> ~/.bash_profile
+    fi
+else
+    echo "source ~/.bashrc" > ~/.bash_profile
+fi
+
+if ! grep -q "export TRACKGIT_PATH=" ~/.bashrc; then
+    echo "export TRACKGIT_PATH='$TRACKGIT_PATH'" >> ~/.bashrc
+else
+    echo "TRACKGIT_PATH is already exported in ~/.bashrc"
+fi
+
+source ~/.bashrc
+
 chmod +x ./CommitManager/Scripts/setup.post.hooks.sh
 ./CommitManager/Scripts/setup.post.hooks.sh
+go build
 go run main.go
